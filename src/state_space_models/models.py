2c462d58acc41c4a736ec0c29d734cc467d153e7
Eric Denovellis <edeno@bu.edu>
2024-06-12 10:25:01 -0700

    Fix variable names, add more docstring information

--- src/state_space_models/models.py
+++ src/state_space_models/models.py
@@ -7,28 +7,25 @@ def log_receptive_field_model(position: jnp.ndarray, params: jnp.ndarray) -> jnp
     return log_max_rate - (position - place_field_center) ** 2 / (2 * scale**2)
 
 
-def receptive_field_model(position: jnp.ndarray, params: jnp.ndarray) -> jnp.ndarray:
-    return jnp.exp(log_receptive_field_model(position, params))
-
-
+# NOTE: most general form of the SSPPF accounts for multiple neurons which is not implemented here
 def stochastic_point_process_filter(
-    init_mean_params: jnp.ndarray,
-    init_variance_params: jnp.ndarray,
+    init_mode_params: jnp.ndarray,
+    init_covariance_params: jnp.ndarray,
     x: jnp.ndarray,
     spike_indicator: jnp.ndarray,
     dt: float,
     transition_matrix: jnp.ndarray,
-    covariance_matrix: jnp.ndarray,
+    latent_state_covariance: jnp.ndarray,
     log_receptive_field_model: callable,
 ) -> tuple[jnp.ndarray, jnp.ndarray]:
     """Stochastic State Point Process Filter (SSPPF)
 
     Parameters
     ----------
-    init_mean_params : jnp.ndarray, shape (n_params,)
+    init_mode_params : jnp.ndarray, shape (n_params,)
         Initial mean parameters
-    init_variance_params : jnp.ndarray, shape (n_params, n_params)
-        Initial variance parameters
+    init_covariance_params : jnp.ndarray, shape (n_params, n_params)
+        Initial covariance parameters
     x : jnp.ndarray, shape (n_time,)
         Continuous-valued input signal
     spike_indicator : jnp.ndarray, shape (n_time,)
@@ -36,18 +33,18 @@ def stochastic_point_process_filter(
     dt : float
         Time step
     transition_matrix : jnp.ndarray, shape (n_params, n_params)
-    covariance_matrix : jnp.ndarray, shape (n_params, n_params)
+    latent_state_covariance : jnp.ndarray, shape (n_params, n_params)
     log_receptive_field_model : callable
         Function that takes in `x` and parameters and returns the log spike rate
 
     Returns
     -------
-    posterior_mean : jnp.ndarray, shape (n_time, n_params)
-    posterior_variance : jnp.ndarray, shape (n_time, n_params, n_params)
+    posterior_mode : jnp.ndarray, shape (n_time, n_params)
+    posterior_covariance : jnp.ndarray, shape (n_time, n_params, n_params)
 
     References
     ----------
-    ...[1] Eden, U. T., Frank, L. M., Barbieri, R., Solo, V. & Brown, E. N.
+    .. [1] Eden, U. T., Frank, L. M., Barbieri, R., Solo, V. & Brown, E. N.
       Dynamic Analysis of Neural Encoding by Point Process Adaptive Filtering.
       Neural Computation 16, 971-998 (2004).
 
@@ -68,18 +65,18 @@ def stochastic_point_process_filter(
         Q : covariance matrix
         \theta_{k | k-1} :
         W_{k | k-1}: one_step_variance_params
-        \theta_{k | k} : posterior_mean
+        \theta_{k | k} : posterior_mode
         W_{k | k} : posterior_variance
         """
 
         # Unpack previous parameters
-        mean_prev, variance_prev = params_prev
+        mode_prev, covariance_prev = params_prev
         x_t, spike_indicator_t = args
 
         # One-step prediction
-        one_step_mean = transition_matrix @ mean_prev
+        one_step_mean = transition_matrix @ mode_prev
         one_step_variance = (
-            transition_matrix @ variance_prev @ transition_matrix.T + covariance_matrix
+            transition_matrix @ covariance_prev @ transition_matrix.T + latent_state_covariance
         )
 
         # Compute the conditional intensity and innovation
@@ -92,24 +89,24 @@ def stochastic_point_process_filter(
         one_step_grad = grad_log_receptive_field_model(x_t, one_step_mean)[None]
         one_step_hess = hess_log_receptive_field_model(x_t, one_step_mean)
 
-        inverse_posterior_variance = (
+        inverse_posterior_covariance = (
             jnp.linalg.pinv(one_step_variance)
             + (one_step_grad.T * conditional_intensity @ one_step_grad)
             - innovation * one_step_hess
         )
-        posterior_variance = jnp.linalg.pinv(inverse_posterior_variance)
-        posterior_mean = one_step_mean + posterior_variance @ (
+        posterior_covariance = jnp.linalg.pinv(inverse_posterior_covariance)
+        posterior_mode = one_step_mean + posterior_covariance @ (
             one_step_grad.squeeze() * innovation
         )
 
-        return (posterior_mean, posterior_variance), (
-            posterior_mean,
-            posterior_variance,
+        return (posterior_mode, posterior_covariance), (
+            posterior_mode,
+            posterior_covariance,
         )
 
     # Run the SSPPF
     return jax.lax.scan(
-        _update, (init_mean_params, init_variance_params), (x, spike_indicator)
+        _update, (init_mode_params, init_covariance_params), (x, spike_indicator)
     )[1]
 
 
@@ -139,19 +136,37 @@ def steepest_descent_point_process_filter(
 
     Returns
     -------
-    posterior_mean : jnp.ndarray, shape (n_time, n_params)
+    posterior_mode : jnp.ndarray, shape (n_time, n_params)
+
+    References
+    ----------
+    .. [1] Brown, E.N., Nguyen, D.P., Frank, L.M., Wilson, M.A., and Solo, V. (2001).
+    An analysis of neural receptive field plasticity by point process adaptive filtering.
+    Proceedings of the National Academy of Sciences 98, 12261–12266.
+    https://doi.org/10.1073/pnas.201409398.
+
+    .. [2] Eden, U. T., Frank, L. M., Barbieri, R., Solo, V. & Brown, E. N.
+      Dynamic Analysis of Neural Encoding by Point Process Adaptive Filtering.
+      Neural Computation 16, 971-998 (2004).
+
+    Notes
+    -----
+    Equation in [1] is for the likelihood while in [2] it is for the log likelihood.
+    This implementation follows the formulation in [2].
+
     """
     grad_log_receptive_field_model = jax.grad(log_receptive_field_model, argnums=1)
 
-    def _update(mean_prev: jnp.ndarray, args: tuple[jnp.ndarray, jnp.ndarray]) -> tuple[jnp.ndarray, jnp.ndarray]:
+    def _update(
+        mode_prev: jnp.ndarray, args: tuple[jnp.ndarray, jnp.ndarray]
+    ) -> tuple[jnp.ndarray, jnp.ndarray]:
         """Steepest Descent Point Process Filter update step"""
-
         x_t, spike_indicator_t = args
-        conditional_intensity = jnp.exp(log_receptive_field_model(x_t, mean_prev)) * dt
+        conditional_intensity = jnp.exp(log_receptive_field_model(x_t, mode_prev)) * dt
         innovation = spike_indicator_t - conditional_intensity
-        one_step_grad = grad_log_receptive_field_model(x_t, mean_prev)
-        posterior_mean = mean_prev + epsilon @ one_step_grad * innovation
+        one_step_grad = grad_log_receptive_field_model(x_t, mode_prev)
+        posterior_mode = mode_prev + epsilon @ one_step_grad * innovation
 
-        return posterior_mean, posterior_mean
+        return posterior_mode, posterior_mode
 
     return jax.lax.scan(_update, init_mean_params, (x, spike_indicator))[1]
--- src/state_space_models/simulate_data.py
+++ src/state_space_models/simulate_data.py
@@ -1,13 +1,12 @@
 import numpy as np
 
 
-def log_receptive_field_model(position: np.ndarray, params: np.ndarray) -> np.ndarray:
-    log_max_rate, place_field_center, scale = params
-    return log_max_rate - (position - place_field_center) ** 2 / (2 * scale**2)
-
-
 def receptive_field_model(position: np.ndarray, params: np.ndarray) -> np.ndarray:
-    return np.exp(log_receptive_field_model(position, params))
+    if params.ndim == 1:
+        params = params[None]
+    log_max_rate, place_field_center, scale = params.T
+    return np.exp(log_max_rate - (position - place_field_center) ** 2 / (2 * scale**2))
+
 
 
 def simulate_eden_brown_2004_jump() -> tuple[np.ndarray, np.ndarray, np.ndarray, float, np.ndarray, np.ndarray]:
@@ -35,3 +34,34 @@ def simulate_eden_brown_2004_jump() -> tuple[np.ndarray, np.ndarray, np.ndarray,
     spike_indicator = np.random.poisson(true_rate * dt)
 
     return time, position, spike_indicator, dt, true_params1, true_params2
+
+
+def simulate_eden_brown_2004_linear():
+    dt = 0.020  # seconds
+    total_time = 8000.0  # seconds
+    n_total_steps = int(total_time / dt)
+
+    time = np.arange(0, total_time, dt)
+
+    speed = 125.0  # cm/s
+    track_length = 300.0  # cm
+
+    run1 = np.arange(0, track_length, speed * dt)
+    run2 = np.arange(track_length, 0, -speed * dt)
+    run = np.concatenate((run1, run2))
+
+    position = np.concatenate([run] * int(np.ceil(n_total_steps / run.shape[0])))
+    position = position[:n_total_steps]
+
+    true_params1 = np.array([np.log(10.0), 250.0, np.sqrt(12.0)])
+    true_params2 = np.array([np.log(30.0), 150.0, np.sqrt(20.0)])
+
+    # Interpolate between true_params1 and true_params2
+    true_params = np.linspace(true_params1, true_params2, n_total_steps)
+    log_max_rate, place_field_center, scale = true_params.T
+    true_rate = np.exp(
+        log_max_rate - (position - place_field_center) ** 2 / (2 * scale**2)
+    )
+    spike_indicator = np.random.poisson(true_rate * dt)
+
+    return time, position, spike_indicator, dt, true_params
