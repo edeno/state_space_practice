1f4ca2ec9f049bbc7c5dc1732500bda5b94025d4
Eric Denovellis <edeno@bu.edu>
2025-05-27 11:25:32 -0400

    Update docstring and typing

--- src/state_space_practice/utils.py
+++ src/state_space_practice/utils.py
@@ -1,5 +1,3 @@
-from typing import Tuple
-
 import numpy as np
 
 
@@ -7,7 +5,7 @@ def check_converged(
     log_likelihood: float,
     previous_log_likelihood: float,
     tolerance: float = 1e-4,
-) -> Tuple[bool, bool]:
+) -> tuple[bool, bool]:
     """We have converged if the slope of the log-likelihood function falls below 'tolerance',
 
     i.e., |f(t) - f(t-1)| / avg < tolerance,
@@ -25,15 +23,16 @@ def check_converged(
     Returns
     -------
     is_converged : bool
+        True if the relative change < tolerance.
     is_increasing : bool
+        True if log-likelihood did not decrease by more than tolerance.
 
     """
     delta_log_likelihood = abs(log_likelihood - previous_log_likelihood)
-    avg_log_likelihood = (
-        abs(log_likelihood) + abs(previous_log_likelihood) + np.spacing(1)
-    ) / 2
+    eps = np.finfo(float).eps
+    avg_log_likelihood = (abs(log_likelihood) + abs(previous_log_likelihood) + eps) / 2
 
-    is_increasing = log_likelihood - previous_log_likelihood >= -1e-3
+    is_increasing = log_likelihood - previous_log_likelihood >= -tolerance
     is_converged = (delta_log_likelihood / avg_log_likelihood) < tolerance
 
-    return is_converged, is_increasing
+    return bool(is_converged), bool(is_increasing)
