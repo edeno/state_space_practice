120369ae832a9a34e7cf93eb32fc760a9b1ce7d5
Eric Denovellis <edeno@bu.edu>
2025-05-29 08:00:29 -0400

    Fix types

--- src/state_space_practice/smith_learning_algorithm.py
+++ src/state_space_practice/smith_learning_algorithm.py
@@ -105,60 +105,58 @@ def approximate_gaussian(
     return mode, covariance
 
 
-@jax.jit
-def log_posterior_objective(
-    learning_state: float,
+@partial(jax.jit, static_argnames=("max_possible_correct", "bias"))
+def _log_posterior_objective(
+    learning_state: jax.Array,
     learning_state_prev: float,
-    sig_sq_old: float,
-    n_correct_in_trial: jnp.ndarray,
+    variance_prev: float,
+    n_correct_in_trial: int,
     max_possible_correct: int,
     bias: float,
-):
-    """Objective function for the log posterior distribution.
+) -> jax.Array:
+    """Objective function for the log posterior distribution at one step.
 
     Parameters
     ----------
-    learning_state : float
-        Current latent learning state estimate, x_k
+    learning_state : jnp.ndarray, shape (1,)
+        Current latent learning state estimate, $x_k$.
     learning_state_prev : float
-        Previous latent learning state estimate vector, x_{k-1}
-    sig_sq_old : float
-        Previous state variance, \sigma_{k-1}^2
-    n_correct_in_trial : jnp.ndarray, shape (n,)
-        Number of correct responses in the trial
+        Previous latent learning state estimate, $x_{k-1}$.
+    variance_prev : float
+        Previous state variance, $P_{k|k-1}$.
+    n_correct_in_trial : int
+        Number of correct responses in the trial, $y_k$.
     max_possible_correct : int
-        Maximum number of correct responses in the trial
+        Maximum number of correct responses, $N_k$.
     bias : float
-        Bias term for the observation model
+        Bias term ($\mu$) for the observation model.
 
     Returns
     -------
     log_posterior : jnp.ndarray, shape (n,)
         Log posterior of the state estimate vector
     """
-    prob_sucess = jax.nn.sigmoid(
+    prob_success = jax.nn.sigmoid(
         bias + learning_state
     )  # can add in covariates @ learning_state
     log_likelihood = jax.scipy.stats.binom.logpmf(
-        k=n_correct_in_trial, n=max_possible_correct, p=prob_sucess
+        k=n_correct_in_trial, n=max_possible_correct, p=prob_success
     )
     log_prior = jax.scipy.stats.norm.logpdf(
-        x=learning_state,
-        loc=learning_state_prev,
-        scale=jnp.sqrt(sig_sq_old),
+        x=learning_state, loc=learning_state_prev, scale=jnp.sqrt(variance_prev)
     )
 
     return jnp.squeeze(log_likelihood + log_prior)
 
 
 def smith_learning_filter(
-    n_correct_responses: jnp.ndarray,
+    n_correct_responses: jax.Array,
     init_learning_state: float = 0.0,
     init_learning_variance: Optional[float] = None,
     sigma_epsilon: float = jnp.sqrt(0.05),
     prob_correct_by_chance: float = 0.5,
     max_possible_correct: Optional[int] = None,
-) -> tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray]:
+) -> Tuple[jax.Array, jax.Array, jax.Array, jax.Array, jax.Array]:
     """Applies a non-linear Bayesian filter (Laplace approximation) for learning.
 
     Assumes a random walk model for the latent learning state ($x_k$) and
@@ -175,18 +173,18 @@ def smith_learning_filter(
     init_learning_state : float, optional
         The subject's learning state at the beginning of the experiment ($x_0$).
         When None, it is set to 0.
-    init_learning_variance : float
+    init_learning_variance : float, optional
         Initial learning state variance ($P_0$). Defaults to $\sigma_\epsilon^2$.
         Controls how fast the learning state is updated.
     sigma_epsilon : float, optional
-        Standard deviation of the process noise ($\sigma_\epsilon$),
+        Standard deviation of process noise ($\sigma_\epsilon$), defaults to sqrt(0.05).
     prob_correct_by_chance : float, optional
         The probability of a correct response by chance in absence of any
         learning or experience, used to set the bias.
-        Default is 0.5.
+        Chance probability ($p_{chance}$), defaults to 0.5.
     max_possible_correct : float, optional
         Maximum number of correct responses in each trial ($N_k$).
-        When None, it is set to the maximum value in `n_correct`.
+        Defaults to max(n_correct_responses).
 
     Returns
     -------
@@ -197,9 +195,9 @@ def smith_learning_filter(
     learning_state_variance : jnp.ndarray, shape (n_trials,)
         Posterior variance of the learning state ($P_{k|k}$).
     one_step_mode : jnp.ndarray, shape (n_trials,)
-        One-step prediction of the learning state in each trial ($x_{k|k-1}$).
+        One-step prediction mode ($x_{k|k-1}$).
     one_step_variance : jnp.ndarray, shape (n_trials,)
-        One-step prediction of the variance of the learning state in each trial ($P_{k|k-1}$).
+        One-step prediction variance ($P_{k|k-1}$).
     """
     # Bias term based on chance probability of correct response
     mu: float = jnp.log(prob_correct_by_chance / (1 - prob_correct_by_chance))
@@ -219,13 +217,13 @@ def smith_learning_filter(
             jnp.ones_like(n_correct_responses, dtype=int) * n_correct_responses.max()
         )
 
-    # @jax.jit
+    @jax.jit
     def _step(
-        params_prev: tuple[float, float], args: tuple[jnp.ndarray, jnp.ndarray]
-    ) -> tuple[tuple[jnp.ndarray, jnp.ndarray], tuple[jnp.ndarray, jnp.ndarray]]:
+        carry: Tuple[float, float], trial_data: Tuple[int, int]
+    ) -> Tuple[Tuple[float, float], Tuple[float, float, float, float]]:
         """A single step of the non-linear filter."""
-        mode_prev, variance_prev = params_prev
-        n_correct_trial_k, max_possible_correct_trial_k = args
+        mode_prev, variance_prev = carry
+        n_correct_trial_k, max_possible_correct_trial_k = trial_data
 
         # 1. Prediction Step
         one_step_mode = mode_prev  # no transition matrix
@@ -233,9 +231,9 @@ def smith_learning_filter(
 
         # 2. Update Step (using Laplace Approximation)
         log_objective_func = partial(
-            log_posterior_objective,
+            _log_posterior_objective,
             learning_state_prev=one_step_mode,
-            sig_sq_old=one_step_variance,
+            variance_prev=one_step_variance,
             n_correct_in_trial=n_correct_trial_k,
             max_possible_correct=max_possible_correct_trial_k,
             bias=mu,
@@ -255,12 +253,15 @@ def smith_learning_filter(
         )
 
     # Run the filter over all trials
-    learning_state_mode, learning_state_variance, one_step_mode, one_step_variance = (
-        jax.lax.scan(
-            _step,
-            (init_learning_state, init_learning_variance),
-            (n_correct_responses, max_possible_correct),
-        )[1]
+    init_carry = (init_learning_state, init_learning_variance)
+    inputs = (n_correct_responses, max_possible_correct)
+    _, output = jax.lax.scan(
+        _step,
+        init_carry,
+        inputs,
+    )
+    (learning_state_mode, learning_state_variance, one_step_mode, one_step_variance) = (
+        output
     )
 
     # Compute probability of correct response
@@ -276,23 +277,23 @@ def smith_learning_filter(
 
 
 def smith_learning_smoother(
-    filtered_learning_state_mode: jnp.ndarray,
-    filtered_learning_state_variance: jnp.ndarray,
-    one_step_mode: jnp.ndarray,
-    one_step_variance: jnp.ndarray,
+    filtered_learning_state_mode: jax.Array,
+    filtered_learning_state_variance: jax.Array,
+    one_step_mode: jax.Array,
+    one_step_variance: jax.Array,
     prob_correct_by_chance: float = 0.5,
-) -> tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray]:
+) -> tuple[jax.Array, jax.Array, jax.Array, jax.Array]:
     """Smooth the filtered learning state estimates using the Kalman smoother.
 
     Parameters
     ----------
-    filtered_learning_state_mode : jnp.ndarray, shape (n_trials,)
+    filtered_learning_state_mode : jax.Array, shape (n_trials,)
         Learning state mode estimates from the filter
-    filtered_learning_state_variance : jnp.ndarray, shape (n_trials,)
+    filtered_learning_state_variance : jax.Array, shape (n_trials,)
         Learning state variance estimates from the filter
-    one_step_mode : jnp.ndarray, shape (n_trials,)
+    one_step_mode : jax.Array, shape (n_trials,)
         One-step prediction of the learning state
-    one_step_variance : jnp.ndarray, shape (n_trials,)
+    one_step_variance : jax.Array, shape (n_trials,)
         One-step prediction of the variance of the learning state
     prob_correct_by_chance : float, optional
         The probability of a correct response by chance in absence of any
@@ -301,26 +302,32 @@ def smith_learning_smoother(
 
     Returns
     -------
-    learning_state_mode : jnp.ndarray, shape (n_trials,)
+    learning_state_mode : jax.Array, shape (n_trials,)
         Smoothed learning state mode estimates
-    learning_state_variance : jnp.ndarray, shape (n_trials,)
+    learning_state_variance : jax.Array, shape (n_trials,)
         Smoothed learning state variance estimates
-    prob_correct_response : jnp.ndarray, shape (n_trials,)
+    prob_correct_response : jax.Array, shape (n_trials,)
         Smoothed probability of a correct response
-    smoother_gain : jnp.ndarray, shape (n_trials,)
+    smoother_gain : jax.Array, shape (n_trials,)
         Smoother gain estimates
     """
+    n_trials: int = len(filtered_learning_state_mode)
+    mu: float = jnp.log(prob_correct_by_chance / (1 - prob_correct_by_chance))
 
-    def _step(params_prev, k):
-        mode_smoothed_prev, variance_smoothed_prev = params_prev
+    @jax.jit
+    def _step(
+        carry: Tuple[float, float], k: int
+    ) -> Tuple[Tuple[float, float], Tuple[float, float, float]]:
+        """A single JIT-compiled step of the RTS smoother."""
+        mode_smoothed_next, variance_smoothed_next = carry
         smoother_gain = (
             filtered_learning_state_variance[k] / one_step_variance[k + 1]
         )  # smoother gain, A_k
         mode_smoothed = filtered_learning_state_mode[k] + smoother_gain * (
-            mode_smoothed_prev - one_step_mode[k + 1]
+            mode_smoothed_next - one_step_mode[k + 1]
         )
         variance_smoothed = filtered_learning_state_variance[k] + smoother_gain**2 * (
-            variance_smoothed_prev - one_step_variance[k + 1]
+            variance_smoothed_next - one_step_variance[k + 1]
         )
 
         return (mode_smoothed, variance_smoothed), (
@@ -334,15 +341,20 @@ def smith_learning_smoother(
         filtered_learning_state_variance[-1],
     )
     n_trials = len(filtered_learning_state_mode)
-    (_, _), (learning_state_mode, learning_state_variance, smoother_gain) = (
-        jax.lax.scan(
-            _step,
-            init_params,
-            jnp.arange(n_trials - 1),
-            reverse=True,
-        )
+    _, output = jax.lax.scan(
+        _step,
+        init_params,
+        jnp.arange(n_trials - 1),
+        reverse=True,
     )
 
+    (
+        learning_state_mode,
+        learning_state_variance,
+        smoother_gain,
+    ) = output
+
+    # Append the last state to the smoothed estimates
     learning_state_mode = jnp.concatenate(
         [learning_state_mode, filtered_learning_state_mode[-1:]]
     )
