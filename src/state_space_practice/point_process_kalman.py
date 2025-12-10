Fix beta calculation in Kalman maximization step
Corrects the computation of beta by including the sum of outer products of consecutive smoother means. This improves the accuracy of the transition matrix estimation in the Kalman maximization step.

src/state_space_practice/point_process_kalman.py
@@ -317,7 +317,10 @@ def kalman_maximization_step(
     )
     gamma1 = gamma - jnp.outer(smoother_mean[-1], smoother_mean[-1]) - smoother_cov[-1]
     gamma2 = gamma - jnp.outer(smoother_mean[0], smoother_mean[0]) - smoother_cov[0]
-    beta = smoother_cross_cov.sum(axis=0).T
+    beta = (
+        smoother_cross_cov.sum(axis=0)
+        + sum_of_outer_products(smoother_mean[:-1], smoother_mean[1:])
+    ).T
 
     # Transition matrix
     transition_matrix = psd_solve(gamma1, beta.T).T